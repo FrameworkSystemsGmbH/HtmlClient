1cb38dd8a96ca246d4c1c5f453f354223c0fa451
Rebitzer <dre@nv.local>
2018-04-04 13:35:11 +0200

    import rxjs delay removed

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -25,7 +25,6 @@ import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/empty';
 import 'rxjs/add/observable/fromEvent';
 
-import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/share';
