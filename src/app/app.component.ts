48fa061e4c39eb3701b448a2d36201ae86eade6a
Rebitzer <dre@nv.local>
2018-04-05 14:38:19 +0200

    iOS menu bar overlap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,43 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 import { FocusService } from 'app/services/focus.service';
 import { LocaleService } from 'app/services/locale.service';
 import { KeyboardService } from 'app/services/keyboard.service';
+import { PlatformService } from 'app/services/platform.service';
 import { SerializeService } from './services/serialize.service';
+import { StyleUtil } from 'app/util/style-util';
 
 @Component({
   selector: 'hc-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  public style: any;
 
   constructor(
     private focusService: FocusService,
     private localeService: LocaleService,
     private keyboardService: KeyboardService,
+    private platformService: PlatformService,
     private serializeService: SerializeService
   ) {
     this.localeService.setMomentLocaleGlobally();
     this.keyboardService.attachScrollHandler();
     this.serializeService.attachHandlers();
+  }
+
+  public ngOnInit(): void {
+    if (this.platformService.isIos()) {
+      this.style = this.createIosStyle();
+    }
+  }
+
+  private createIosStyle(): any {
+    return {
+      'padding-top.px': 20
+    };
   }
 
   @HostListener('window:keydown', ['$event'])
