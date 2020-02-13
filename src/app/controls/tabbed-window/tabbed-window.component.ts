4fe9f1cb344749948317208315654d3686481519
DRE <dominik.rebitzer@framework-systems.de>
2020-02-13 15:57:07 +0100

    tabbed window continuous scrolling

--- src/app/controls/tabbed-window/tabbed-window.component.ts
+++ src/app/controls/tabbed-window/tabbed-window.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, ViewContainerRef, OnInit, Output, EventEmitter, Renderer2, NgZone, ElementRef } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, OnInit, Output, EventEmitter, Renderer2, NgZone, ElementRef, ChangeDetectorRef } from '@angular/core';
+import { animate, transition, trigger, style } from '@angular/animations';
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-ngx';
 
 import { ILayoutableProperties } from 'app/layout/layoutable-properties.interface';
@@ -10,11 +11,32 @@ import { TabPageWrapper } from 'app/wrappers/tabbed-window/tab-page-wrapper';
 import { TabPageTemplate } from 'app/wrappers/tabbed-window/tab-page-template';
 import { TabAlignment } from 'app/enums/tab-alignment';
 import { Visibility } from 'app/enums/visibility';
+import { PlatformService } from 'app/services/platform/platform.service';
 
 @Component({
   selector: 'hc-tabbed-window',
   templateUrl: './tabbed-window.component.html',
-  styleUrls: ['./tabbed-window.component.scss']
+  styleUrls: ['./tabbed-window.component.scss'],
+  animations: [
+    trigger('arrow', [
+      transition('void => *', [
+        style({
+          opacity: 0
+        }),
+        animate('0.25s', style({
+          opacity: 1
+        }))
+      ]),
+      transition('* => void', [
+        style({
+          opacity: 1
+        }),
+        animate('0.25s', style({
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class TabbedWindowComponent extends ContainerComponent implements OnInit {
 
@@ -37,6 +59,15 @@ export class TabbedWindowComponent extends ContainerComponent implements OnInit 
   public headerStyle: any;
   public tabsStyle: any;
   public contentStyle: any;
+  public arrowStyle: any;
+
+  public arrowLeftVisible: boolean;
+  public arrowRightVisible: boolean;
+
+  private scrollLeftInterval: any;
+  private scrollRightInterval: any;
+
+  private scrollDelta: number = 100;
 
   public scrollerOptions: any = {
     className: 'os-theme-thin-dark',
@@ -57,8 +88,8 @@ export class TabbedWindowComponent extends ContainerComponent implements OnInit 
   };
 
   constructor(
-    private zone: NgZone,
-    private renderer: Renderer2
+    private platformService: PlatformService,
+    private cdr: ChangeDetectorRef
   ) {
     super();
   }
@@ -100,6 +131,7 @@ export class TabbedWindowComponent extends ContainerComponent implements OnInit 
     this.headerStyle = this.createHeaderStyle();
     this.tabsStyle = this.createTabsStyle();
     this.contentStyle = this.createContentStyle(wrapper);
+    this.arrowStyle = this.createArrowStyle(wrapper);
     this.scrollerOptions = this.createScrollerOptions();
   }
 
@@ -158,7 +190,7 @@ export class TabbedWindowComponent extends ContainerComponent implements OnInit 
     const layoutHeight: number = layoutableProperties.getClientHeight();
     const isVisible: boolean = this.isVisible && layoutWidth > 0 && layoutHeight > 0;
 
-    let style: any = {
+    let wrapperStyle: any = {
       'display': isVisible ? 'flex' : 'none',
       'left.rem': StyleUtil.pixToRem(layoutableProperties.getX()),
       'top.rem': StyleUtil.pixToRem(layoutableProperties.getY()),
@@ -185,20 +217,20 @@ export class TabbedWindowComponent extends ContainerComponent implements OnInit 
 
     switch (this.tabAlignment) {
       case TabAlignment.Left:
-        style = { ...style, 'flex-direction': 'row' };
+        wrapperStyle = { ...wrapperStyle, 'flex-direction': 'row' };
         break;
       case TabAlignment.Right:
-        style = { ...style, 'flex-direction': 'row-reverse' };
+        wrapperStyle = { ...wrapperStyle, 'flex-direction': 'row-reverse' };
         break;
       case TabAlignment.Bottom:
-        style = { ...style, 'flex-direction': 'column-reverse' };
+        wrapperStyle = { ...wrapperStyle, 'flex-direction': 'column-reverse' };
         break;
       default:
-        style = { ...style, 'flex-direction': 'column' };
+        wrapperStyle = { ...wrapperStyle, 'flex-direction': 'column' };
         break;
     }
 
-    return style;
+    return wrapperStyle;
   }
 
   protected createHeaderStyle(): any {
@@ -227,7 +259,7 @@ export class TabbedWindowComponent extends ContainerComponent implements OnInit 
   }
 
   protected createContentStyle(wrapper: TabbedWindowWrapper): any {
-    let style: any = {
+    let contentStyle: any = {
       'background-color': wrapper.getBackColor(),
       'border-style': 'solid',
       'border-color': wrapper.getBorderColor(),
@@ -247,25 +279,31 @@ export class TabbedWindowComponent extends ContainerComponent implements OnInit 
 
     switch (this.tabAlignment) {
       case TabAlignment.Left:
-        style = { ...style, 'margin-left.rem': margin };
+        contentStyle = { ...contentStyle, 'margin-left.rem': margin };
         break;
       case TabAlignment.Right:
-        style = { ...style, 'margin-right.rem': margin };
+        contentStyle = { ...contentStyle, 'margin-right.rem': margin };
         break;
       case TabAlignment.Bottom:
-        style = { ...style, 'margin-bottom.rem': margin };
+        contentStyle = { ...contentStyle, 'margin-bottom.rem': margin };
         break;
       default:
-        style = { ...style, 'margin-top.rem': margin };
+        contentStyle = { ...contentStyle, 'margin-top.rem': margin };
         break;
     }
 
-    return style;
+    return contentStyle;
+  }
+
+  protected createArrowStyle(wrapper: TabbedWindowWrapper): any {
+
   }
 
   protected createScrollerOptions(): any {
+    let newOptions: any = this.scrollerOptions;
+
     if (this.tabAlignment === TabAlignment.Top || this.tabAlignment === TabAlignment.Bottom) {
-      return {
+      newOptions = {
         ...this.scrollerOptions,
         overflowBehavior: {
           x: 'scroll',
@@ -273,7 +311,7 @@ export class TabbedWindowComponent extends ContainerComponent implements OnInit 
         }
       };
     } else {
-      return {
+      newOptions = {
         ...this.scrollerOptions,
         overflowBehavior: {
           x: 'hidden',
@@ -281,29 +319,81 @@ export class TabbedWindowComponent extends ContainerComponent implements OnInit 
         }
       };
     }
+
+    if (this.platformService.isMobile()) {
+      newOptions = {
+        ...this.scrollerOptions,
+        scrollbars: {
+          autoHide: 'scroll',
+          autoHideDelay: 250
+        }
+      };
+    } else {
+      newOptions = {
+        ...this.scrollerOptions,
+        scrollbars: {
+          autoHide: 'move',
+          autoHideDelay: 250
+        }
+      };
+    }
+
+    return newOptions;
+  }
+
+  public startScrollingLeft(event: any): void {
+    if (event.button === 0) {
+      const value: string = `-= ${this.scrollDelta}px`;
+      this.scrollHorizontal(value);
+      this.scrollLeftInterval = setInterval(() => { this.scrollHorizontal(value); }, 100);
+    }
+  }
+
+  public stopScrollingLeft(): void {
+    clearInterval(this.scrollLeftInterval);
+  }
+
+  public startScrollingRight(event: any): void {
+    if (event.button === 0) {
+      const value: string = `+= ${this.scrollDelta}px`;
+      this.scrollHorizontal(value);
+      this.scrollRightInterval = setInterval(() => { this.scrollHorizontal(value); }, 100);
+    }
+  }
+
+  public stopScrollingRight(): void {
+    clearInterval(this.scrollRightInterval);
+  }
+
+  protected scrollHorizontal(value: string): void {
+    this.scroller.osInstance().scroll({ x: value }, 100);
   }
 
   public refreshScroller(): void {
-    this.zone.runOutsideAngular(() => {
-      if (this.scroller != null && this.scroller.osInstance() != null) {
-        const overflow: number = this.scroller.osInstance().getState().overflowAmount.x;
-        if (overflow > 0) {
-          const scrollPos: number = this.scroller.osInstance().getElements().viewport.scrollLeft;
-          if (scrollPos === 0) {
-            this.renderer.addClass(this.scroller.osTarget(), 'arrowRight');
-            this.renderer.removeClass(this.scroller.osTarget(), 'arrowLeft');
-          } else if (scrollPos >= overflow) {
-            this.renderer.addClass(this.scroller.osTarget(), 'arrowLeft');
-            this.renderer.removeClass(this.scroller.osTarget(), 'arrowRight');
-          } else {
-            this.renderer.addClass(this.scroller.osTarget(), 'arrowLeft');
-            this.renderer.addClass(this.scroller.osTarget(), 'arrowRight');
-          }
+    if (this.scroller != null && this.scroller.osInstance() != null) {
+      const overflow: number = this.scroller.osInstance().getState().overflowAmount.x;
+      if (overflow > 0) {
+        const scrollPos: number = this.scroller.osInstance().getElements().viewport.scrollLeft;
+        if (scrollPos === 0) {
+          this.stopScrollingLeft();
+          this.arrowLeftVisible = false;
+          this.arrowRightVisible = true;
+        } else if (scrollPos >= overflow) {
+          this.stopScrollingRight();
+          this.arrowLeftVisible = true;
+          this.arrowRightVisible = false;
         } else {
-          this.renderer.removeClass(this.scroller.osTarget(), 'arrowLeft');
-          this.renderer.removeClass(this.scroller.osTarget(), 'arrowRight');
+          this.arrowLeftVisible = true;
+          this.arrowRightVisible = true;
         }
+      } else {
+        this.stopScrollingLeft();
+        this.stopScrollingRight();
+        this.arrowLeftVisible = false;
+        this.arrowRightVisible = false;
       }
-    });
+
+      this.cdr.detectChanges();
+    }
   }
 }
