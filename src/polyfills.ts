02fd65083dee4aa5a5bafb13c182fa52802cbc84
DRE <dominik.rebitzer@framework-systems.de>
2020-06-30 11:35:44 +0200

    tsconfig baseurl

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -3,8 +3,8 @@ import 'web-animations-js';
 import 'zone.js/dist/zone';
 
 // Extensions
-import 'ext/array/array';
-import 'ext/boolean/boolean';
-import 'ext/math/math';
-import 'ext/number/number';
-import 'ext/string/string';
+import './ext/array/array';
+import './ext/boolean/boolean';
+import './ext/math/math';
+import './ext/number/number';
+import './ext/string/string';
