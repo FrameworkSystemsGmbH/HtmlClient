daa4fdd4948b3648a8bb85a7835e2de8ca03d1cf
DRE <dominik.rebitzer@framework-systems.de>
2020-08-10 15:36:21 +0200

    error / message / retry box footer focus

--- src/app/components/errorbox/errorbox.component.ts
+++ src/app/components/errorbox/errorbox.component.ts
@@ -1,6 +1,5 @@
-import { Component, Inject, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { Component, Inject, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Subscription } from 'rxjs';
 
 import { IErrorBoxData } from 'app/components/errorbox/errorbox-data.interface';
 import { BackService } from 'app/services/back-service';
@@ -11,7 +10,7 @@ import { BackButtonPriority } from 'app/enums/backbutton-priority';
   templateUrl: './errorbox.component.html',
   styleUrls: ['./errorbox.component.scss']
 })
-export class ErrorBoxComponent implements OnInit, OnDestroy {
+export class ErrorBoxComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @ViewChild('footer', { static: true })
   public footer: ElementRef;
@@ -21,7 +20,6 @@ export class ErrorBoxComponent implements OnInit, OnDestroy {
   public stackTrace: string;
   public showStackTrace: boolean;
 
-  private afterOpenSub: Subscription;
   private onBackButtonListener: () => boolean;
 
   constructor(
@@ -37,18 +35,14 @@ export class ErrorBoxComponent implements OnInit, OnDestroy {
   public ngOnInit(): void {
     this.onBackButtonListener = this.onBackButton.bind(this);
     this.backService.addBackButtonListener(this.onBackButtonListener, BackButtonPriority.Overlay);
+  }
 
-    this.afterOpenSub = this.dialogRef.afterOpened().subscribe(() => {
-      setTimeout(() => this.footer.nativeElement.focus());
-    });
+  public ngAfterViewInit(): void {
+    this.footer.nativeElement.focus();
   }
 
   public ngOnDestroy(): void {
     this.backService.removeBackButtonListener(this.onBackButtonListener);
-
-    if (this.afterOpenSub) {
-      this.afterOpenSub.unsubscribe();
-    }
   }
 
   private onBackButton(): boolean {
