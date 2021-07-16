820dd74a88c21d89c53df76a5444a4c3e2a69af5
DRE <dominik.rebitzer@framework-systems.de>
2021-07-16 10:21:07 +0200

    #9 - API agnostic layout configuration

--- android/app/src/main/java/com/fs/htmlclient/MainActivity.java
+++ android/app/src/main/java/com/fs/htmlclient/MainActivity.java
@@ -4,6 +4,8 @@ import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 
@@ -21,42 +23,46 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     registerPlugin(WebViewCachePlugin.class);
-
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
-      this.hideSystemUI();
-    }
+    this.hideSystemUI();
   }
 
   @Override
   protected void onPostCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
     super.onPostCreate(savedInstanceState);
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-      this.hideSystemUI();
-    }
-
     this.attachSizeObserver();
   }
 
   private void hideSystemUI() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-      getWindow().addFlags(WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      getWindow().setDecorFitsSystemWindows(true);
+
+      final WindowInsetsController insetsCtrl = getWindow().getDecorView().getWindowInsetsController();
+
+      if (insetsCtrl != null) {
+        insetsCtrl.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        insetsCtrl.hide(WindowInsets.Type.systemBars());
+      }
     }
 
-    getWindow().getDecorView().setFitsSystemWindows(true);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+      final WindowManager.LayoutParams params = getWindow().getAttributes();
+      params.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER;
+      getWindow().setAttributes(params);
+    }
 
-    final int flags = (
-      View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        | View.SYSTEM_UI_FLAG_FULLSCREEN
-        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-    );
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+      final int flags = (
+        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+          | View.SYSTEM_UI_FLAG_FULLSCREEN
+          | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+          | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+          | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+          | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+      );
 
-    getWindow().getDecorView().setSystemUiVisibility(flags);
+      getWindow().getDecorView().setSystemUiVisibility(flags);
+    }
   }
 
   private void attachSizeObserver() {
