e2324420a558da2f2c6079c5f5641b545703f93a
DRE <dominik.rebitzer@framework-systems.de>
2021-07-21 14:54:15 +0200

    #12 - viewdocument action fsbroker url scheme

--- src/app/services/actions/viewdoc.service.ts
+++ src/app/services/actions/viewdoc.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
+import { IAppState } from '@app/store/app.state';
+import { selectBrokerFilesUrl } from '@app/store/broker/broker.selectors';
 import { Browser } from '@capacitor/browser';
+import { Store } from '@ngrx/store';
 
 @Injectable({ providedIn: 'root' })
 export class ViewDocService {
 
+  private readonly brokerScheme: string = 'fsbroker://';
+
+  private _brokerUrl: string | null = null;
+
+  public constructor(store: Store<IAppState>) {
+    store.select(selectBrokerFilesUrl).subscribe(filesUrl => {
+      this._brokerUrl = filesUrl;
+    });
+  }
+
   public viewDocument(url: string): void {
     if (url.trim().length > 0) {
-      void Browser.open({ url });
+      let newUrl: string = url;
+
+      if (this._brokerUrl != null && newUrl.startsWith(this.brokerScheme)) {
+        newUrl = `${this._brokerUrl}/${newUrl.trimStringLeft(this.brokerScheme)}`;
+      }
+
+      void Browser.open({ url: newUrl });
     }
   }
 }
